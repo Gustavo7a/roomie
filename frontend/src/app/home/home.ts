3774e8cd4935193d602e41e0fd4b830e9886232c
Lucas Henrique <lucash.062004@gmail.com>
2026-02-19 04:47:12 -0300

    finaliza componente de buscae filtros
    Edição de estilo da Home e criação da aba de pesquisa, assim como aplicação do painel de filtros com captura de parâmetros na URL.
    
    Nota de Integração: A exibição dos cards vai ser conectada só depois do PR #51 for aceito e mergeado para evitar conflitos O array de resultados já está sendo recebido com sucesso.

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,18 +1,100 @@
-import {Component, inject} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {Auth} from '../auth/auth';
-import {Router} from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../auth/auth';
+import { Router, ActivatedRoute } from '@angular/router'; 
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { PropertyService } from './property.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
   private auth = inject(Auth);
   private router = inject(Router);
+  private route = inject(ActivatedRoute); 
+  private fb = inject(FormBuilder);
+  
+  private propertyService = inject(PropertyService);
+
+  hasSearched: boolean = false; 
+  appliedLocation: string = ''; 
+  
+  initialSearch = new FormControl(''); 
+
+  filterForm: FormGroup = this.fb.group({
+    location: [''],
+    minPrice: [''],
+    maxPrice: [''],
+    propertyType: ['']
+  });
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (Object.keys(params).length > 0) {
+        this.hasSearched = true;
+        this.filterForm.patchValue(params);
+        this.appliedLocation = params['location'] || ''; 
+      }
+    });
+  }
+
+  onInitialSearch() {
+    if (this.initialSearch.value) {
+      this.filterForm.patchValue({ location: this.initialSearch.value });
+    }
+    this.hasSearched = true;
+    this.onFilter();
+  }
+
+  onFilter() {
+    const formValues = this.filterForm.value;
+    
+    this.appliedLocation = formValues.location; 
+
+    const cleanParams: any = {};
+    Object.keys(formValues).forEach(key => {
+      if (formValues[key] !== null && formValues[key] !== '') {
+        cleanParams[key] = formValues[key];
+      }
+    });
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: cleanParams
+    });
+
+    this.propertyService.buscarComFiltros(cleanParams).subscribe({
+      next: (resultados) => {
+        console.log('Imóveis encontrados no Banco de Dados:', resultados);
+
+      },
+      error: (err) => {
+        console.error('Erro ao buscar imóveis:', err);
+      }
+    });
+  }
+
+
+  goBackHome() {
+    this.hasSearched = false;
+    this.appliedLocation = ''; 
+    this.initialSearch.reset();
+    this.filterForm.reset();
+    
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {}
+    });
+  }
+
+  goToProfile() {
+    alert('Aqui abrirá o Perfil!');
+  }
 
   onLogout(): void {
     this.auth.logout();
